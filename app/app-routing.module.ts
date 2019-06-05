730a2fa7a45a18bb203ad630febd780c594324aa
Brian Poston <bkposton@gmail.com>
2019-06-05 13:47:01 -0400

    added stubs for classes and dashboard

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -7,7 +7,9 @@ const routes: Routes = [
     { path: "", redirectTo: "/welcome", pathMatch: "full" },
     { path: "welcome", loadChildren: "./welcome/welcome.module#WelcomeModule" },
     { path: "home", loadChildren: "./home/home.module#HomeModule" },
-    { path: "login", loadChildren: "./login/login.module#LoginModule" }
+    { path: "login", loadChildren: "./login/login.module#LoginModule" },
+    { path: "dashboard", loadChildren: "./dashboard/dashboard.module#DashboardModule" },
+    { path: "classes", loadChildren: "./classes/classes.module#ClassesModule" },
 ];
 
 @NgModule({
